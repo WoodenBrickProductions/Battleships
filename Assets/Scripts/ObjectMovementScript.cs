350812750f89883055eabda7f8d8cae4c5316104
WoodenBrickProductions <tautvisceponis@gmail.com>
2021-05-23 22:46:16 +0300

    Updated movement and spawning scripts

--- Assets/Scripts/ObjectMovementScript.cs
+++ Assets/Scripts/ObjectMovementScript.cs
@@ -7,7 +7,12 @@ public class ObjectMovementScript : MonoBehaviour
     
     [SerializeField] private float amplitude = 0.0001f;
     [SerializeField] private float frequency = 1f;
-
+    [SerializeField] private bool bob = false;
+    [SerializeField] private bool rotate = false;
+    
+    private Vector3 _direction;
+    private Vector3 _rotationDirection;
+    private bool _setDirection = false;
     private float randomOffset;
     // Start is called before the first frame update
     void Start()
@@ -15,11 +20,43 @@ public class ObjectMovementScript : MonoBehaviour
         randomOffset = Random.value;
     }
 
+    public void SetMovementDirection(Vector3 direction)
+    {
+        _direction = direction;
+        _setDirection = true;
+    }
+
+    public void SetRotationDirection(Vector3 direction)
+    {
+        _rotationDirection = direction;
+        rotate = true;
+    }
+
+    private void Move()
+    {
+        transform.position += _direction * Time.deltaTime;
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        Vector3 currentPosition = transform.position;
-        currentPosition.y += Mathf.Sin ((Time.fixedTime + randomOffset) * Mathf.PI * frequency) * amplitude;
-        transform.position = currentPosition;
+        if (_setDirection)
+        {
+            Move();
+        }
+
+        if (bob)
+        {
+            Vector3 currentPosition = transform.position;
+            currentPosition.y += Mathf.Sin ((Time.fixedTime + randomOffset) * Mathf.PI * frequency) * amplitude;
+            transform.position = currentPosition;
+        }
+
+        if (rotate)
+        {
+            Vector3 rotation = transform.rotation.eulerAngles;
+            rotation += _rotationDirection * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(rotation);
+        }
     }
 }
--- Assets/Scripts/ObjectSpawningScript.cs
+++ Assets/Scripts/ObjectSpawningScript.cs
@@ -1,29 +1,59 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ObjectSpawningScript : MonoBehaviour
 {
     [SerializeField] private GameObject spawnable;
-    [SerializeField] private int intervalMin = 1;
-    [SerializeField] private int intervalMax = 2;
-
+    [SerializeField] private int intervalMin = 10;
+    [SerializeField] private int intervalMax = 20;
+    [SerializeField] private Vector3 startingDirection;
+    [SerializeField] private Vector3 startingRotation;
+    
     [SerializeField] private float speedMin = 1;
     [SerializeField] private float speedMax = 2;
+    [SerializeField] private float lifetime = 20;
 
-    private float remainingTime;
+    [SerializeField] private BoxCollider boxCollider;
+    
+    
+    private ObjectMovementScript _movementScript;
+
+    private void Start()
+    {
+        boxCollider = GetComponentInChildren<BoxCollider>();
+        print(boxCollider.bounds.center);
+    }
+
+    [SerializeField] private float remainingTime;
     // Update is called once per frame
     void Update()
     {
         if (remainingTime <= 0)
         {
-            
-            
-            remainingTime = Random.value * (intervalMax - intervalMin) + intervalMin;
+            GameObject instance = Instantiate(spawnable);
+            _movementScript = instance.GetComponentInChildren<ObjectMovementScript>();
+            _movementScript.SetMovementDirection(startingDirection * Random.Range(speedMin, speedMax));
+            _movementScript.SetRotationDirection(startingRotation * Random.Range(speedMin, speedMax));
+            remainingTime = Random.Range(intervalMin, intervalMax);
+            print(RandomPointInBounds(boxCollider.bounds));
+            instance.transform.position = RandomPointInBounds(boxCollider.bounds);
+            Destroy(instance, lifetime);
         }
         else
         {
             remainingTime -= Time.deltaTime;
         }
     }
+    
+    public static Vector3 RandomPointInBounds(Bounds bounds) {
+        return new Vector3(
+            Random.Range(bounds.min.x, bounds.max.x),
+            Random.Range(bounds.min.y, bounds.max.y),
+            Random.Range(bounds.min.z, bounds.max.z)
+        );
+    }
+
 }
